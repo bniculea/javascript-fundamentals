57e8b7088006a82d403db2cd0b6972fed58bedb8
bogdan.niculeasa <niculeasab@haufe.com>
2024-07-18 09:25:49 +0300

    add more structure + content

---
+++ code/2. variables-and-datatypes/index.js
@@ -0,0 +1,10 @@
+
+// var variable is hoisted and it will not complain
+// that age is not defined, it will just log undefined
+console.log(age);
+var age = 28;
+console.log(age)
+
+
+// now it will complain that username is not defined
+console.log(username)
