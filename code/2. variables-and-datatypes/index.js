5887104f27442f8d787f10eb379ad680f1fd27aa
bogdan.niculeasa <niculeasab@haufe.com>
2024-07-20 12:21:06 +0300

    add more content for the variables and datatypes

--- code/2. variables-and-datatypes/index.js
+++ code/2. variables-and-datatypes/index.js
@@ -1,4 +1,6 @@
 
+
+//region  var, let and const
 // var variable is hoisted and it will not complain
 // that age is not defined, it will just log undefined
 console.log(age);
@@ -7,4 +9,21 @@ console.log(age)
 
 
 // now it will complain that username is not defined
-console.log(username)
+//console.log(username)
+
+// console.log(length);
+// let length;
+
+// const
+
+const myConst = 2.56;
+if (myConst === 2.56) {
+    const myConst = 3.99;
+    console.log(myConst);
+}
+console.log(myConst);
+
+
+for(let n in numbers) {
+    console.log(n)
+}
