25fbbb384c0c273078bcdcba411b12d22e336b36
bniculea <bogdan.niculeasa@hotmail.com>
2024-07-21 10:41:29 +0300

    add content about objects - partil

--- code/2. variables-and-datatypes/index.js
+++ code/2. variables-and-datatypes/index.js
@@ -16,14 +16,32 @@ console.log(age)
 
 // const
 
-const myConst = 2.56;
-if (myConst === 2.56) {
-    const myConst = 3.99;
-    console.log(myConst);
-}
-console.log(myConst);
+// const myConst = 2.56;
+// if (myConst === 2.56) {
+//     const myConst = 3.99;
+//     console.log(myConst);
+// }
+// console.log(myConst);
 
 
-for(let n in numbers) {
-    console.log(n)
+// for(let n in numbers) {
+//     console.log(n)
+// }
+
+const pet = {
+    type: 'dog',
+    breed: 'Malinois',
+    age: 3 
 }
+
+// Statement #1
+// pet = {
+//     type: 'cat',
+//     age: 2
+// }
+
+// Statement #2
+pet.age = 1.4
+
+// Statement #3
+pet.type = 4.5
