20ee74d463d2fe2c424084c34ed0833ec21a6744
bniculea <bogdan.niculeasa@hotmail.com>
2024-07-21 12:00:00 +0300

    finish variables and datatypes section

--- code/2. variables-and-datatypes/index.js
+++ code/2. variables-and-datatypes/index.js
@@ -1,47 +1,11 @@
-
-
-//region  var, let and const
-// var variable is hoisted and it will not complain
-// that age is not defined, it will just log undefined
-console.log(age);
-var age = 28;
-console.log(age)
-
-
-// now it will complain that username is not defined
-//console.log(username)
-
-// console.log(length);
-// let length;
-
-// const
-
-// const myConst = 2.56;
-// if (myConst === 2.56) {
-//     const myConst = 3.99;
-//     console.log(myConst);
-// }
-// console.log(myConst);
-
-
-// for(let n in numbers) {
-//     console.log(n)
-// }
-
-const pet = {
+let pet = {
     type: 'dog',
-    breed: 'Malinois',
-    age: 3 
+    breed: 'malinois'
 }
 
-// Statement #1
-// pet = {
-//     type: 'cat',
-//     age: 2
-// }
+let myDog = pet
+myDog.age = 3;
+myDog.breed = 'Doberman'
 
-// Statement #2
-pet.age = 1.4
-
-// Statement #3
-pet.type = 4.5
+console.log(pet.age);
+console.log(pet.breed);
